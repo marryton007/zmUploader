1301cc23b309bfa49cc6791ff5eaab61f1981115
marryton007 <marryton007@163.com>
2015-05-25 23:25:20 +0800

    make a simplest example

--- static/client/manual-trigger.js
+++ static/client/manual-trigger.js
@@ -1,23 +1,21 @@
-var manualUploader = new qq.FineUploader({
-    element: document.getElementById('fine-uploader-manual-trigger'),
-    template: 'qq-template',
-    request: {
-        endpoint: '/server/success.html',
-        method: 'GET' // Only for the gh-pages demo website due to Github Pages limitations
-    },
-    text: {
-        uploadButton: '<div class="icon-upload icon-white"></div> Click me now and upload a product image'
+function createUpLoader () {
+    var manualUploader = new qq.FineUploader({
+        element: document.getElementById('fine-uploader-manual-trigger'),
+        template: 'qq-template-manual-trigger',
+        request: {
+            endpoint: '/uploads'
+        //method: 'GET' // Only for the gh-pages demo website due to Github Pages limitations
     },
     thumbnails: {
         placeholders: {
-            waitingPath: '/source/placeholders/waiting-generic.png',
-            notAvailablePath: '/source/placeholders/not_available-generic.png'
+            waitingPath: 'client/placeholders/waiting-generic.png',
+            notAvailablePath: 'client/placeholders/not_available-generic.png'
         }
     },
-    autoUpload: false,
+    autoUpload: true,
     debug: true
-});
+    });
 
-qq(document.getElementById("trigger-upload")).attach("click", function() {
-    manualUploader.uploadStoredFiles();
-});
+}
+
+window.onload = createUpLoader;
